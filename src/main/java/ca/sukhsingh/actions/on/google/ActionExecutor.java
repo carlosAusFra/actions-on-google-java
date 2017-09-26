3911c873b3ad51545e99f487abdea8cca640cfe2
Sukhvinder Singh <sukhvindersingh02@gmail.com>
2017-09-26 09:16:25 -0400

    adding javadocs

--- src/main/java/ca/sukhsingh/actions/on/google/ActionExecutor.java
+++ src/main/java/ca/sukhsingh/actions/on/google/ActionExecutor.java
@@ -6,29 +6,47 @@ import ca.sukhsingh.actions.on.google.response.Response;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
+/**
+ * Action executor is a helper class which elemenate the use of actions to check which method of call.
+ * With help of action executor, user will be able to call a method name similar to the action name.
+ * Only thing to worry about is, make sure you method name and action name is exactly same and also case senstive
+ */
 public class ActionExecutor {
 
-    Object methodRepository;
+    /**
+     * a repository which will hold all the methods from user side
+     */
+    Object actionRepository;
 
-    public ActionExecutor(Object methodRepository) {
-        this.methodRepository = methodRepository;
+    /**
+     * A constructor dependency injection is being used here.
+     * wherever user initiate ActionExecutor object the repository is required
+     * @param actionRepository {@link Object} where all the methods are defined
+     */
+    public ActionExecutor(Object actionRepository) {
+        this.actionRepository = actionRepository;
     }
 
+    /**
+     * This method will bring the response from the service class
+     * for e.g. Response rs = executor.getResponse(request);
+     * @param request {@link Request} a request object
+     * @return {@link Response} a user constructed response object
+     */
     public Response getResponse(Request request) {
         Method m = null;
         try {
-            m = methodRepository.getClass().getMethod(request.getAction(),Request.class);
+            m = actionRepository.getClass().getMethod(request.getAction(),Request.class);
         } catch (NoSuchMethodException e) {
             e.printStackTrace();
         }
         try {
-            return (Response)m.invoke(methodRepository, request);
+            return (Response)m.invoke(actionRepository, request);
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         } catch (InvocationTargetException e) {
             e.printStackTrace();
         }
-
         return null;
     }
 }
